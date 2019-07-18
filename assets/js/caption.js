30cdaee46c777449f4a97a22ec072d1e11d8bdc6
Akshath Kothari <akshath.kothari@gmail.com>
2019-07-18 21:28:20 +0530

    Change caption

--- assets/js/caption.js
+++ assets/js/caption.js
@@ -1,5 +1,5 @@
 (function(){
-    var captions = ["That's my name.", "Welcome to my humble abode.", "How you doin'?", "Want to work together? Contact me."];
+    var captions = ["That's my name.", "Welcome to my humble abode.", "How you doin'?", "Wanna work together? Contact me."];
     var rand_caption = captions[Math.floor(Math.random() * captions.length)];
     document.getElementById("caption").innerHTML = rand_caption;
 })();
